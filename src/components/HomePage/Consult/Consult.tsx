146f58c589ed76c2547c2435fb1a6358d5895238
VadymDenysiuk <80970410371q@gmail.com>
2022-07-02 17:17:27 +0300

    create section Consult

---
+++ src/components/HomePage/Consult/Consult.tsx
@@ -0,0 +1,23 @@
+import LeftLine from '../../../assets/svg/promo-left-line.svg'
+import RightLine from '../../../assets/svg/promo-right-line.svg'
+
+const Consult = () => {
+
+  return (
+    <section className='consult'>
+      <div className="container">
+        <div className="consult__text">
+          <h2 className="title">БЕСПЛАТНАЯ консультация с экспертом</h2>
+          <p className="consult__description section__text">
+          Получите ответы на все ваши вопросы! 
+        </p>
+        </div>
+        <form className="consult__form">
+          
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Consult
