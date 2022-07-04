c9970cc56244ab0c1abd77c9afc599340d200358
VadymDenysiuk <80970410371q@gmail.com>
2022-07-04 12:36:15 +0300

    complete section Promo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Promo from '../src/components/HomePage/Promo/Promo'
-
+import Consult from '../src/components/HomePage/Consult/Consult'
 
 const IndexPage: NextPage = () => {
   return (
     <>
      <Promo/>
+     <Consult/>
     </>
   )
 }
--- src/components/HomePage/Promo/Promo.tsx
+++ src/components/HomePage/Promo/Promo.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import LeftLine from '../../../assets/svg/promo-left-line.svg'
 import RightLine from '../../../assets/svg/promo-right-line.svg'
 
@@ -6,17 +7,28 @@ const Promo = () => {
   return (
     <section className='promo'>
       <div className="container promo__container">
-        <h1 className="promo__title">Все что нужно для вашего автомобиля</h1>
+        <div className="promo__bg"></div>
+        <h1 className="promo__title">Все, що потрібно для вашого автомобіля</h1>
         <div className="promo__description">
-          <p className="promo__description__item  section__text">Расходники обслуживания авто.<br/> От винтика до масла <LeftLine className="promo__description__item--left"/></p>
+          <p className="promo__description__item  section__text"> <span className='orange-text'>Витратні матеріали</span> обслуговування авто. Від гвинтика до олії <LeftLine className="promo__description__item--left"/></p>
           <p className="promo__description__item section__text"> 
           <RightLine className="promo__description__item--right"/>
-          Кузовные запчасти для авто из США, Европы, Кореи и других стран</p>
+          <span className='orange-text'>Кузовні запчастини</span> для авто з США, Європи, КореЇ та інших країн</p>
         </div>
         <div className="promo__buttons">
-          <button className="promo__buttons__item--left section__text">Узнать цены</button>
-          <button className="promo__buttons__item--right section__text">Получить дефектовку Бесплатно</button>
+          <button className="promo__buttons__item--left section__text">Дізнатися ціни</button>
+          <button className="promo__buttons__item--right section__text">Отримати дефектування безкоштовно</button>
         </div>
+        
+          <div className="promo__bg--image">
+            <Image
+              className=""
+              src="/assets/images/auto-promo.png"
+              layout="fill"
+              objectFit="cover"
+              alt='car'
+              />
+          </div>
       </div>
     </section>
   )
