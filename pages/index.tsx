c647ac9500b7ac31399134ef59c4e898009cfae6
VadymDenysiuk <80970410371q@gmail.com>
2022-07-04 17:01:40 +0300

    complete section Danger

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Promo from '../src/components/HomePage/Promo/Promo'
 import Consult from '../src/components/HomePage/Consult/Consult'
 import Services from '../src/components/HomePage/Services/Services'
+import Danger from '../src/components/HomePage/Danger/Danger'
+
 
 const IndexPage: NextPage = () => {
   return (
@@ -9,6 +11,7 @@ const IndexPage: NextPage = () => {
      <Promo/>
      <Consult/>
      <Services/>
+     <Danger/>
     </>
   )
 }
---
+++ src/components/HomePage/Danger/Danger.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import Piston from '../../../assets/svg/danger-piston.svg'
+import Car from '../../../assets/svg/danger-car.svg'
+import Shield from '../../../assets/svg/danger-shield.svg'
+
+const Danger = () => {
+
+  return (
+    <section className='danger'>
+      <div className="container danger__container">
+        <h2 className='title danger__title'>Небезпеки, від яких ви будете вбережені, працюючи з нами</h2>
+        <div className="danger__cards">
+          <div className="danger__cards__item">
+            <div className="danger__cards__item-svg">
+              <Piston/>
+            </div>
+            <p className='section__text danger__cards__item-text'>
+              Ми знаємо різницю в запчастинах американських авто та європейських аналогах, що <span className='orange-text'>виключить неприємні сюрпризи та додаткові витрати</span> 
+            </p>
+          </div>
+          <div className="danger__cards__item">
+            <div className="danger__cards__item-svg">
+              <Car/>
+            </div>
+            <p className='section__text danger__cards__item-text'>
+            <span className='orange-text'>Ми не припустимо</span>, щоб продавець підсунув неоригінал, який не підійде на Ваш автомобіль
+            </p>
+          </div>
+          <div className="danger__cards__item">
+            <div className="danger__cards__item-svg">
+              <Shield/>
+            </div>
+            <p className='section__text danger__cards__item-text'>
+            Ринок запчастин славиться великою кількістю шахраїв, працюючи з нами  <span className='orange-text'>ви будете захищені</span> від фінансових втрат
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Danger
