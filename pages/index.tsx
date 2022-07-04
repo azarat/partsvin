947de7fec0686c00df982f488017ca5b1b49b02b
VadymDenysiuk <80970410371q@gmail.com>
2022-07-04 16:10:33 +0300

    complete section Services

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Promo from '../src/components/HomePage/Promo/Promo'
 import Consult from '../src/components/HomePage/Consult/Consult'
+import Services from '../src/components/HomePage/Services/Services'
 
 const IndexPage: NextPage = () => {
   return (
     <>
      <Promo/>
      <Consult/>
+     <Services/>
     </>
   )
 }
---
+++ src/constants/aboutUs.ts
@@ -0,0 +1,37 @@
+import TeamSVG from '../../public/assets/images/svg/team.svg'
+import CoinsSVG from '../../public/assets/images/svg/coins.svg'
+import ShieldSVG from '../../public/assets/images/svg/shield.svg'
+import CarSVG from '../../public/assets/images/svg/solid-car.svg'
+
+const aboutUs = [
+  {
+    id: 1,
+    title: 'Команда',
+    icon: TeamSVG,
+    content:
+      'С вами будут работать профессионалы! Средний опыт наших сотрудников в автотематике 5,5 лет',
+  },
+  {
+    id: 2,
+    title: 'Экономия',
+    icon: CoinsSVG,
+    content:
+      'В среднем, на покупке автомобиля мы сэкономили 4113$ каждому клиенту!',
+  },
+  {
+    id: 3,
+    title: 'Качество',
+    icon: ShieldSVG,
+    content:
+      'Перед покупкой автомобиля, мы проводим более 10 проверок по различным сервисам',
+  },
+  {
+    id: 4,
+    title: 'Ассортимент',
+    icon: CarSVG,
+    content:
+      'На американском автомобильном рынке каждый день продаются тысячи автомобилей. Благодаря этому, мы можем предложить от 10-ти вариантов в месяц, которые совпадают с вашими пожеланиями. Вы сможете выбрать самый подходящий и лучший вариант!',
+  },
+]
+
+export default aboutUs
