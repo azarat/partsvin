7e4bfc11e623f2faf8ee60a263cdce237a574aed
VadymDenysiuk <80970410371q@gmail.com>
2022-07-07 16:38:09 +0300

    adaptive Clients

--- src/components/HomePage/Clients/Clients.tsx
+++ src/components/HomePage/Clients/Clients.tsx
@@ -1,8 +1,10 @@
 import  ClientsCars  from '../../../constants/clients'
 import { ImgComparisonSlider } from '@img-comparison-slider/react';
+import { useState } from 'react';
+import ArrowSVG from '../../../assets/svg/arrow-review.svg'
 
 const Clients = () => {
- 
+  const [active, setActive] = useState(false)
 
   return (
     <section className='clients'>
@@ -11,7 +13,7 @@ const Clients = () => {
 
         <ul className="clients__cards">
           {ClientsCars.map(({ id, model, image_1, image_2 }) => (
-            <li key={id} className="clients__cards__item">
+            <li key={id} className={"clients__cards__item" + (active == true ? " odd" : " even") }>
               <div className="clients__cards__item--image">
               <ImgComparisonSlider>
                 <img slot="first" src={image_1}  />
@@ -23,6 +25,8 @@ const Clients = () => {
             </li>
           ))
           }
+          <button className='clients__btn' onClick={() => {setActive(!active) 
+            console.log('qwe')} }><ArrowSVG/></button>
         </ul>
       </div>
     </section>
