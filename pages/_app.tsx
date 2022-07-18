f729e7a074e5f243421368c29a377ba35ed37124
VadymDenysiuk <80970410371q@gmail.com>
2022-07-18 18:32:35 +0300

    add google-analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,23 @@ import Footer from '../src/components/Footer/Footer'
 import Header from '../src/components/Header/Header'
 import '../styles/main.scss'
 import { hotjar } from 'react-hotjar'
+import { useRouter } from 'next/router'
+import * as ga from './lib/ga'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
 
   useEffect(() => {
-    hotjar.initialize(215523203, 1)
-  }, [])
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     
