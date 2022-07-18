1ad4dcc85f10cdc6eaea96f3e232a5dc5bc683fa
VadymDenysiuk <80970410371q@gmail.com>
2022-07-18 17:02:04 +0300

    add hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,16 @@ import { useEffect } from 'react'
 import Footer from '../src/components/Footer/Footer'
 import Header from '../src/components/Header/Header'
 import '../styles/main.scss'
+import { hotjar } from 'react-hotjar'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
+  useEffect(() => {
+    hotjar.initialize(215523203, 1)
+  }, [])
+
   return (
+    
     <>
       <Head>
         <title>
