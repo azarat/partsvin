c9af9f1654b1c22d1207258cebc0baa88e7348e5
VadymDenysiuk <80970410371q@gmail.com>
2022-07-18 16:52:45 +0300

    fix faq

--- src/components/HomePage/Faq/Faq.tsx
+++ src/components/HomePage/Faq/Faq.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import  FaqItems  from '../../../constants/faq'
- 
+import Accordion from 'react-bootstrap/Accordion';
+
 const Faq = () => {
   const [activeFaq, setActiveFaq] = useState<number | null>(null)
   
@@ -8,19 +9,17 @@ const Faq = () => {
     <section className='faq'>
       <div className="container faq__container">
         <h2 className='title faq__title'>відповіді на часті запитання</h2>
-        <ul className="faq__accordion">
-          {FaqItems.map(({ id, question, answer }, i) => (
-            <li key={id} className={`faq__accordion-item ${
-              activeFaq === i ? 'faq__accordion-item--open' : ''
-            }`}
-            onClick={() => setActiveFaq(activeFaq === i ? null : i)}
-            role="presentation">
-              <p  className='faq__accordion-item-title'>{question}</p>
-              <p  className='section__text faq__accordion-item-text'>{answer}</p>
-            </li>
-          ))
+        <Accordion defaultActiveKey="0" >
+          {FaqItems.map(({ id, question, answer }) => (
+              <Accordion.Item key={id} eventKey={id} >
+              <Accordion.Header>{question}</Accordion.Header>
+              <Accordion.Body>
+                {answer}
+              </Accordion.Body>
+            </Accordion.Item>
+            ))
           }
-        </ul>
+        </Accordion>
       </div>
     </section>
   )
