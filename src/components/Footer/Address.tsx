dcce63c6717bdb31ccdb75ce955be0d95b85de1e
VadymDenysiuk <80970410371q@gmail.com>
2022-07-26 14:30:43 +0300

    fix networks

--- src/components/Footer/Address.tsx
+++ src/components/Footer/Address.tsx
@@ -6,7 +6,7 @@ const Address = () => {
       <div className='footer__address'>
         <p className='footer__address--text'>Україна, Київ, Коломийський перевулок 20</p>
         <p className='footer__address--text'>sale@partsvin.net</p>
-        <p className='footer__address--text'>+380687454269</p>
+        <p className='footer__address--text'>+380681748181</p>
       </div>
   )
 }
